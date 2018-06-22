e4d58f06a4b6384fc7b0d32e344868f037a0d517
Alvaro Polo <apoloval@gmail.com>
2018-06-22 18:40:49 +0200

    Remove unused code

--- src/cpu/z80/cpu.rs
+++ src/cpu/z80/cpu.rs
@@ -1,6 +1,4 @@
-use super::data::Data;
 use super::inst::{Context, Inst};
-use super::ops::{Dest, Source};
 use super::regs::Registers;
 
 pub struct CPU {
@@ -15,36 +13,5 @@ impl Context for CPU {
 impl CPU {
     pub fn exec<I: Inst>(&mut self, inst: I) {
         inst.exec(self)
-    }
-
-    fn exec_inc<T: Data>(&mut self, dst: &Dest<T>) {
-        let val = self.read_dst(dst);
-        self.write_dest(dst, T::inc(val));
-    }
-
-    fn exec_nop(&mut self) {}
-
-    fn exec_load<T: Data>(&mut self, dst: &Dest<T>, src: &Source<T>) {
-        let val = self.read_src(src);
-        self.write_dest(dst, val);
-    }
-
-    fn read_src<T: Data>(&self, op: &Source<T>) -> T::Value {
-        match op {
-            Source::Literal(v) => *v,
-            Source::Register(r) => self.regs.read::<T>(*r),
-        }
-    }
-
-    fn read_dst<T: Data>(&self, op: &Dest<T>) -> T::Value {
-        match op {
-            Dest::Register(r) => self.regs.read::<T>(*r),
-        }
-    }
-
-    fn write_dest<T: Data>(&mut self, op: &Dest<T>, val: T::Value) {
-        match op {
-            Dest::Register(r) => self.regs.write::<T>(*r, val),
-        }
     }
 }
--- src/cpu/z80/mod.rs
+++ src/cpu/z80/mod.rs
@@ -2,6 +2,5 @@ pub mod cpu;
 pub mod data;
 pub mod inst;
 pub mod mem;
-pub mod ops;
 pub mod regs;
 
