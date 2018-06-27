e0a7d42fe35df989e92f6d5507b9be9733e2aa08
Alvaro Polo <apoloval@gmail.com>
2018-06-27 20:56:14 +0200

    Add props for currently supported instructions

--- src/cpu/z80/props.rs
+++ src/cpu/z80/props.rs
@@ -1,5 +1,4 @@
 use cpu::z80::inst::{Inst, Src, Dest};
-use cpu::z80::regs::{Reg8, Reg16};
 
 pub type InstSize = usize;
 pub type InstTime = usize;
@@ -13,8 +12,17 @@ impl InstProps {
     pub fn from_inst(inst: &Inst) -> InstProps {
         match inst {
             Inst::NOP => InstProps { size: 1, time: 4 },
-            Inst::LD16(Dest::Reg(Reg16::BC), Src::Liter(_)) => InstProps { size: 3, time: 10 },
-            Inst::LD8(Dest::IndReg(Reg16::BC), Src::Reg(Reg8::A)) => InstProps { size: 1, time: 7 },
+            Inst::ADD16(Dest::Reg(_), Src::Reg(_)) => InstProps { size: 1, time: 11 },
+            Inst::DEC8(_) => InstProps { size: 1, time: 4 },
+            Inst::DEC16(_) => InstProps { size: 1, time: 6 },
+            Inst::INC8(_) => InstProps { size: 1, time: 4 },
+            Inst::INC16(_) => InstProps { size: 1, time: 6 },
+            Inst::LD16(Dest::Reg(_), Src::Liter(_)) => InstProps { size: 3, time: 10 },
+            Inst::LD8(Dest::Reg(_), Src::Liter(_)) => InstProps { size: 2, time: 7 },
+            Inst::LD8(Dest::Reg(_), Src::IndReg(_)) => InstProps { size: 1, time: 7 },
+            Inst::LD8(Dest::IndReg(_), Src::Reg(_)) => InstProps { size: 1, time: 7 },
+            Inst::RLCA => InstProps { size: 1, time: 4 },
+            Inst::RRCA => InstProps { size: 1, time: 4 },
             _ => unimplemented!("props of given instruction is not implemented"),
         }
     }
