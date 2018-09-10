12cc1db9d80d5b2750990a151d0d370978abb564
Alvaro Polo <apoloval@gmail.com>
2018-09-10 19:13:00 +0200

    Fix hex format in `assert_result!` macro

--- src/testutil.rs
+++ src/testutil.rs
@@ -3,10 +3,10 @@ macro_rules! assert_result {
         assert_eq!($a, $b, "{} expects {:08b}b but {:08b}b given ", $pre, $a, $b)
     );
     (HEX8, $pre:expr, $a:expr, $b:expr) => (
-        assert_eq!($a, $b, "{} expects {:02x}h but {:02x}h given ", $pre, $a, $b)
+        assert_eq!($a, $b, "{} expects {:02X}h but {:02X}h given ", $pre, $a, $b)
     );
     (HEX16, $pre:expr, $a:expr, $b:expr) => (
-        assert_eq!($a, $b, "{} expects {:04x}h but {:04x}h given ", $pre, $a, $b)
+        assert_eq!($a, $b, "{} expects {:04X}h but {:04X}h given ", $pre, $a, $b)
     );
 }
 
