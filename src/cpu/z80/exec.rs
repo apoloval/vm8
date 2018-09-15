569207a142ca7acb0497283d7ef91100e7a0063a
Alvaro Polo <alvaro.polo@cabify.com>
2018-09-15 19:19:06 +0200

    Z80: Implement "LD L, L"

--- src/cpu/z80/exec.rs
+++ src/cpu/z80/exec.rs
@@ -133,6 +133,7 @@ pub fn exec_step<CTX: Context>(ctx: &mut CTX) -> Cycles {
         0x6a => { ctx.exec_ld::<L, D>();        04 },
         0x6b => { ctx.exec_ld::<L, E>();        04 },
         0x6c => { ctx.exec_ld::<L, H>();        04 },
+        0x6d => { ctx.exec_ld::<L, L>();        04 },
 
         0xc3 => { ctx.exec_jp::<L16>();         10 },
         _ => unimplemented!("cannot execute illegal instruction with opcode 0x{:x}", opcode),
@@ -650,6 +651,7 @@ mod test {
     test_ld_r8_r8!(test_exec_ld_l_d, L, D, l, set_d);
     test_ld_r8_r8!(test_exec_ld_l_e, L, E, l, set_e);
     test_ld_r8_r8!(test_exec_ld_l_h, L, H, l, set_h);
+    test_ld_r8_r8!(test_exec_ld_l_l, L, L, l, set_l);
 
     macro_rules! test_ld_indreg_a {
         ($fname:ident, $regname:ident, $regset:ident) => {
